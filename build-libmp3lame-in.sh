7e703752a547b5142fc9ba69fb5a14e224a67a0d
kongfl888 <kongfl888@outlook.com>
2017-07-01 01:17:08 +0800

    Update build script

--- build-libmp3lame-in.sh
+++ build-libmp3lame-in.sh
@@ -8,9 +8,9 @@
 
 export PKG_CONFIG_PATH=/home/Airlltools/ffmpeg_build/lib/pkgconfig/
 
-./configure --prefix=/home/Airlltools/ffmpeg_build --disable-shared --enable-static --build=mingw32 --enable-nasm --disable-analyzer-hooks --disable-gtktest --disable-mp3x  --disable-mp3rtp  &&
+./configure --prefix=/home/Airlltools/ffmpeg_build --disable-shared --enable-static --build=mingw32 --enable-nasm --disable-analyzer-hooks --disable-gtktest --disable-debug --disable-mp3x  --disable-mp3rtp &&
 
-make -j$NUMBER_OF_PROCESSORS && make install
+make -j$NUMBER_OF_PROCESSORS && make install &&
 
 make distclean > /dev/null 2>&1
 
